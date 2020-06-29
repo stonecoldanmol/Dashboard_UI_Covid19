017c24c3475a1f8c4cf407ab994ee0a1c372c24c
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-06-29 12:06:36 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fluttercoviddashboardui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttercoviddashboardui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:fluttercoviddashboardui/screens/bottom_nav_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Covid-19 Dashboard UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
